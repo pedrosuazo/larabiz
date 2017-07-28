e06fb2b3c1e15bfc1b1eb9a6624c72bd0009154f
Pedro Suazo <suazopedro@gmail.com>
2017-07-28 15:25:31 -0500

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <h3>Your Listings</h3>
+                    @if(count($listings))
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Company</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+
+                            @foreach($listings as $listing)
+                                <tr>
+                                    <td>{{ $listing->name}}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+
+                        </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
